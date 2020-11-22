ea19897b764b828781adc8358ea12d3db9824cb2
Araggik <45770047+Araggik@users.noreply.github.com>
2020-11-22 15:03:50 +0500

    Add files via upload

---
+++ Client/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+
+namespace Client
+{
+    class Client
+    {
+        public static WebSocket client;
+        static void Main(string[] args)
+        {
+            client = new WebSocket("ws://localhost:8080");
+            client.Connect();
+            string s = Console.ReadLine();
+            while(s!="0")
+            {
+                client.Send(s);
+                s = Console.ReadLine();
+            }
+        }
+    }
+}
